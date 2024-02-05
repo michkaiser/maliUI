fb4678b173ae11bc1b93e4ac08dfa2c65e3defb8
michkaiser <michkaiser13@gmail.com>
2024-02-05 20:25:46 +0300

    Initial Commit

---
+++ Mali/MaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaliApp.swift
+//  Mali
+//
+//  Created by Kaiser on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
